1fc0e3eaa370bd6828327d937a48ed0863936a93
Lex <xunleee@qq.com>
2021-06-21 17:42:52 +0800

    1-2

--- main.go
+++ main.go
@@ -6,20 +6,26 @@ import (
 	"net/http"
 )
 
+type Engine struct {
+
+}
+//使用自己的handler，接管所有的http请求，做统一处理
+func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request)  {
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.PATH = %q\n", req.URL.Path)
+	case "/hello":
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w,"404 not found %s\n",req.URL)
+	}
+}
+
 func main() {
 
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
+	engine := new(Engine)
 
-	log.Fatal(http.ListenAndServe(":9999", nil))
-}
-
-func indexHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.PATH = %q\n", req.URL.Path)
-}
-
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-	}
+	log.Fatal(http.ListenAndServe(":9999", engine))
 }
